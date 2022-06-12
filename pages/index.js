0ea970e34e9a576d260cf1284051ab658c7fa893
wlcharlie <wl00606352@gmail.com>
2022-06-12 16:15:30 +0800

    try something new

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,21 @@ import Image from "next/image"
 import Link from "next/link"
 
 import { getSortedPostsData } from "lib/posts"
+import { getSortedNotesData } from "lib/notes"
 
 export async function getStaticProps() {
+  const allNotesData = getSortedNotesData()
   const allPostsData = getSortedPostsData()
+  console.log(allNotesData)
   return {
     props: {
       allPostsData,
+      allNotesData,
     },
   }
 }
-export default function Home({ allPostsData }) {
+
+export default function Home({ allPostsData, allNotesData }) {
   return (
     <div>
       {/* Keep the existing code here */}
@@ -23,13 +28,14 @@ export default function Home({ allPostsData }) {
           {allPostsData.map(({ id, date, title }) => (
             <li key={id}>
               <Link href={`/posts/${id}`}>
-                <a>
-                  {title}
-                  <br />
-                  {id}
-                  <br />
-                  {date}
-                </a>
+                <a>{title}</a>
+              </Link>
+            </li>
+          ))}
+          {allNotesData.map(({ id, title }) => (
+            <li key={id}>
+              <Link href={`/notes/${id}`}>
+                <a>{title || ""}</a>
               </Link>
             </li>
           ))}
---
+++ pages/notes/[id].js
@@ -0,0 +1,30 @@
+import { getAllNoteIds, getNoteData } from "lib/notes"
+
+export async function getStaticPaths() {
+  const paths = getAllNoteIds()
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const noteData = await getNoteData(params.id)
+  return {
+    props: {
+      noteData,
+    },
+  }
+}
+
+export default function Note({ noteData }) {
+  return (
+    <div>
+      {noteData.title}
+      <br />
+      {noteData.id}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: noteData.contentHtml }} />
+    </div>
+  )
+}
